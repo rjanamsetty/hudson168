0fd24f667c84255eb08c0778ad56f36ffc10b9e8
rjanamsetty <janamsetty@gmail.com>
2023-05-17 19:41:29 -0400

    add checking for directory

--- light_sensor.py
+++ light_sensor.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 from waveshare_TSL2591 import TSL2591
@@ -24,13 +25,16 @@ def save_lux():
     Saves the lux value to the drive
     :return: None
     """
-    with open(f"lux_{utils.est_time_now()}.txt", "w") as file:
+    # If images folder does not exist, create it
+    with open(f"lux/lux_{utils.est_time_now()}.txt", "w") as file:
         # Write the integer value to the file
         file.write(str(lux()))
 
 
 if __name__ == '__main__':
     # Take an image every minute
+    if not os.path.exists("lux"):
+        os.makedirs("lux")
     while True:
         save_lux()
         time.sleep(60)
